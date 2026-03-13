3fd5f82dbc9ca1227263b0c37dbd1795d19e4f66
bq406057 <quentin.bonnaffoux06@gmail.com>
2026-03-13 11:40:30 +0100

    lien entre controller et service, devrait fonctionner : a tester avec la BD

--- back/src/main/java/com/g1b/station_back/controller/ProductController.java
+++ back/src/main/java/com/g1b/station_back/controller/ProductController.java
@@ -1,14 +1,25 @@
 package com.g1b.station_back.controller;
 
+import com.g1b.station_back.dto.StockItemDTO;
+import com.g1b.station_back.service.StockService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "*")
 public class ProductController {
+
+    private final StockService stockService;
+
+    public ProductController(StockService stockService) {
+        this.stockService = stockService;
+    }
 
     @GetMapping("/{productId}")
     public Long getProduct(@PathVariable Long productId) {
@@ -16,8 +27,8 @@ public class ProductController {
     }
 
     @GetMapping("/all")
-    public void getAllProducts() {
-
+    public List<StockItemDTO> getAllProducts() {
+        return stockService.getAllStocks();
     }
 
     @GetMapping("/ping")
