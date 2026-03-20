2e0c55c0c9293f93e31376b50d30894d9d895bf9
Mathéo Carli <matheo.carli@etu.unice.fr>
2026-03-20 16:35:20 +0100

    widget chargers connecté à la base de données

--- back/src/main/java/com/g1b/station_back/model/EvCharger.java
+++ back/src/main/java/com/g1b/station_back/model/EvCharger.java
@@ -16,29 +16,19 @@ public class EvCharger {
     @Column(name = "is_fast", nullable = false)
     private Boolean isFast;
 
-    @Column(name = "energy_available", nullable = false, precision = 5, scale = 3)
-    private BigDecimal energyAvailable;
-
-    @Column(name = "alert_threshold", precision = 5, scale = 3)
-    private BigDecimal alertThreshold;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PumpChargerStatus status;
 
     public EvCharger() {}
     public EvCharger(Integer idEvCharger, Boolean isFast, BigDecimal energyAvailable, BigDecimal alertThreshold, PumpChargerStatus status) {
-        this.idEvCharger = idEvCharger; this.isFast = isFast; this.energyAvailable = energyAvailable; this.alertThreshold = alertThreshold; this.status = status;
+        this.idEvCharger = idEvCharger; this.isFast = isFast; this.status = status;
     }
 
     public Integer getIdEvCharger() { return idEvCharger; }
     public void setIdEvCharger(Integer idEvCharger) { this.idEvCharger = idEvCharger; }
     public Boolean getIsFast() { return isFast; }
     public void setIsFast(Boolean isFast) { this.isFast = isFast; }
-    public BigDecimal getEnergyAvailable() { return energyAvailable; }
-    public void setEnergyAvailable(BigDecimal energyAvailable) { this.energyAvailable = energyAvailable; }
-    public BigDecimal getAlertThreshold() { return alertThreshold; }
-    public void setAlertThreshold(BigDecimal alertThreshold) { this.alertThreshold = alertThreshold; }
     public PumpChargerStatus getStatus() { return status; }
     public void setStatus(PumpChargerStatus status) { this.status = status; }
 }
--- front/src/components/Switch.tsx
+++ front/src/components/Switch.tsx
@@ -1,17 +1,20 @@
 import type {FC} from "react";
 
 interface Props {
-    id: string;
-    onClick?: (enable: boolean) => void;
-    enabled?: boolean;
+	id: string;
+	onClick?: (enable: boolean) => void;
+	checked?: boolean;
+	disabled?: boolean;
 }
 
-export const Switch: FC<Props> = ({id, onClick, enabled}: Props) => {
-    
-    return (
-        <div className="toggle-switch">
-            <input className="toggle-input" id={id} type="checkbox" checked={enabled} onChange={(e) => onClick?.(e.target.checked)}/>
-            <label className="toggle-label" htmlFor={id}></label>
-        </div>
-    )
+export const Switch: FC<Props> = ({id, onClick, checked, disabled}: Props) => {
+
+	return (
+		<div className="toggle-switch">
+			<input className="toggle-input" id={id} type="checkbox" checked={checked}
+				   onChange={(e) => onClick?.(e.target.checked)}
+				   disabled={disabled}/>
+			<label className="toggle-label" htmlFor={id}></label>
+		</div>
+	)
 }
